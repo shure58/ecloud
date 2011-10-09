77a44d710a68c09e4a2dd69d3847e6b61df2a07e
shure <bshure58@gmail.com>
2011-10-09 03:15:41 +0300

    Heroku recommit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ecloud::Application.config.secret_token = 'ba01128b214f43a19ad4c4122ea3d813e3076b349ad67244f15548953d1f594f8c2a2dc16671a17a96ff9c4a0b2108424722749b44a8dfbd40c76ee6bdb2afb8'
