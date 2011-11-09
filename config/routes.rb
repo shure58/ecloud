d77912d9d70d59d638de443a31dd0ea3ae6512f0
shure <bshure58@gmail.com>
2011-11-09 12:06:14 +0300

    09112011

--- config/routes.rb
+++ config/routes.rb
@@ -51,7 +51,7 @@ Ecloud::Application.routes.draw do
   #   end
 
   # You can have the root of your site routed with "root"
-  # just remember to delete public/index2.html.
+  # just remember to delete public/x2.html.
   # root :to => "welcome#index"
 
   # See how all your routes lay out with "rake routes"
