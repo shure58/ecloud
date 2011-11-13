a9b27f3f390c424cc311ed7280ce9a9b4604e1b0
shure <bshure58@gmail.com>
2011-11-14 00:14:16 +0300

    13112011

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,8 @@ Ecloud::Application.configure do
   config.serve_static_assets = false
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
+    config.action_controller.asset_host = "http://ecloud.heroku.com"
+  config.action_mailer.default_url_options = {:host => 'http://ecloud.heroku.com'}
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
